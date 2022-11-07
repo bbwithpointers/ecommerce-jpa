94db2e7c7b26e7c1ec8580d6292adf9169b9fc56
Bruno Barros <contato4code@gmail.com>
2022-11-07 07:10:26 -0300

    Separada testes de EntityManager e Produto

--- src/test/java/com/algaworks/iniciandocomjpa/ConsultaRegistrosTest.java
+++ src/test/java/com/algaworks/iniciandocomjpa/ConsultaRegistrosTest.java
@@ -1,40 +1,12 @@
 package com.algaworks.iniciandocomjpa;
 
 import com.algaworks.model.Produto;
-import org.junit.*;
+import org.junit.Test;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import static org.junit.Assert.*;
-
-public class ConsultaRegistrosTest {
-
-    private static EntityManagerFactory entityManagerFactory;
-
-    private EntityManager entityManager;
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        entityManagerFactory.close();
-    }
-
-    @Before
-    public void setUp() {
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @After
-    public void tearDown() {
-        entityManager.close();
-    }
-
+public class ConsultaRegistrosTest extends EntityManagerTest {
 
     @Test
     public void buscarPorId() {
@@ -43,7 +15,7 @@ public class ConsultaRegistrosTest {
     }
 
     @Test
-    public void atualizaReferencia(){
+    public void atualizaReferencia() {
         Produto produto = entityManager.find(Produto.class, 1);
         produto.setNome("Microfone");
 
